08408330e4121c0b75f861f17fe6cc67f98755a4
Glenn <glenniii.dev@gmail.com>
2025-08-21 15:17:54 -0400

    Update Logout.tsx

--- components/Logout.tsx
+++ components/Logout.tsx
@@ -10,7 +10,12 @@ export default function Logout() {
     const pathname = usePathname()
 
     if (!currentUser) {
-        return null
+      return (
+        <a href="mailto:glenniii.dev@gmail.com">
+          <Button text="Contact" />
+            {/* <button type="button" className="border-2 border-[#005247] text-[#005247] hover:border-[#ff8000] hover:text-[#ff8000] px-4 py-2 rounded-full font-bold">Contact</button> */}
+        </a>
+      )
     }
 
     if (pathname === '/') {
