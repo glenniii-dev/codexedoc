a7868cca3bb2f2819fddbc374cdce881541695f1
Glenn Hensley III <glenniii.dev@gmail.com>
2025-09-14 18:57:23 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {},
+  experimental: {
+    globalNotFound: true,
+  },
   images: {
     remotePatterns: [
       {
