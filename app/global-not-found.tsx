7562dea80951b634fde5e35fb0967fedb8524a93
Glenn Hensley III <glenniii.dev@gmail.com>
2025-09-14 20:54:25 -0400

    Adjusted 404 page for mobile

--- app/global-not-found.tsx
+++ app/global-not-found.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
 export default function GlobalNotFound() {
   return (
     <html lang="en">
-      <body className={`bg-black text-white ${sourceCodePro.className} flex items-center justify-center text-center h-screen flex-col p-20 lg:p-40`}>
+      <body className={`bg-black text-white ${sourceCodePro.className} flex items-center justify-center text-center h-screen flex-col p-10 lg:p-40`}>
         <h1 className="text-2xl sm:text-4xl lg:text-6xl font-bold pb-10">404 - Page Not Found</h1>
         <p className="text-lg sm:text-2xl lg:text-3xl">Our dev is too busy applying to jobs that never respond to notice this broken link. Try checking the URL or head back to the <Link href="/" className="underline">homepage</Link>.</p>
       </body>
