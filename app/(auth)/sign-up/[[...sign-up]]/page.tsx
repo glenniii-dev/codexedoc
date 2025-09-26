3da235fc525be8fddfb16c1e0135f44956f9acae
Glenn Hensley III <glenniii.dev@gmail.com>
2025-09-26 13:30:40 -0400

    Adjusted Background image height

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <section className="flex flex-col items-center p-5 md:p-10 bg-[url('/background.jpg')] bg-cover bg-center">
+    <section className="flex flex-col items-center min-h-screen p-5 md:p-10 bg-[url('/background.jpg')] bg-cover bg-center">
       <div className="flex flex-row items-center p-0 m-0">
         <Image src="/logo.png" alt="Codexedoc Logo" width={1000} height={1000} className="w-25 h-25 mr-2" />
         <h1 className="text-5xl font-bold text-white">CODEXEDOC</h1>
